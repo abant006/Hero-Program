754fe7cb278d806605fb538bcd8eb5ba73c28e44
aaronbantay <aaronbantay@gmail.com>
2022-05-19 23:59:53 -0700

    Minor updates: Added a few comments

--- Character.h
+++ Character.h
@@ -25,6 +25,8 @@ class Character {
  	bool isAlive() const; // returns true if getHealth() returns an integer greater than 0, otherwise false
 	
 	// PURE VIRTUAL FUNCTION THAT MUST BE IMPLEMENTED BY DERIVED CLASSES
+	// Polymorphism can also be used for references to objects like seen here in the parameter
+	// Declares a reference that can refer to to Character or derived class objects
  	virtual void attack(Character &) = 0;
  };
 
