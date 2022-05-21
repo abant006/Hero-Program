11d27dacd90fcdf8cb4162bb2e5eb35dfdc33623
AaronBantay <aaronbantay@gmail.com>
2022-05-21 12:36:16 -0700

    Commit for test bench

--- test.cpp
+++ test.cpp
@@ -3,24 +3,35 @@
 #include "Character.h"
 #include "Warrior.h"
 #include "Elf.h"
+#include "Wizard.h"
 
 using namespace std;
 
 int main() {
-    // create 2 Warriors and 2 Elves
-    Warrior w1("Arthur", 100, 6, "King George");
-    Warrior w2("Leo", 100, 9, "King George");
-    Elf e1("Raegron", 100, 4, "Sylvarian");
-    Elf e2("Melimion", 100, 5, "Sylvarian");
 
-    // now have them attack each other
-    w1.attack(e1);
-    cout << endl;
-    w1.attack(w2);
+    // creating instances of the classes
+    Warrior w1("Arthur", 100, 5, "King George");
+    Warrior w2("Jane", 100, 6, "King George");
+    Warrior w3("Bob", 100, 4, "Queen Emily");
+    Elf e1("Raegron", 100, 4, "Sylvarian");
+    Elf e2("Cereasstar", 100, 3, "Sylvarian");
+    Elf e3("Melimion", 100, 4, "Valinorian");
+    Wizard wz1("Merlin", 100, 5, 10);
+    Wizard wz2("Adali", 100, 5, 8);
+    Wizard wz3("Vrydore", 100, 4, 6);
+
+    // testing the different overriden attack functions
+    e1.attack(w1);
     cout << endl;
     e1.attack(e2);
     cout << endl;
-    e1.attack(w2);
+    w2.attack(w1);
+    cout << endl;
+    w3.attack(w1);
+    cout << endl;
+    wz1.attack(wz2);
+    cout << endl;
+    wz1.attack(wz3);
     cout << endl;
 
     return 0;
