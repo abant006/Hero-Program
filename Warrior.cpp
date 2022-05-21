305e25ceda74682901d6f92f6add8a8c5b5854aa
AaronBantay <aaronbantay@gmail.com>
2022-05-20 19:14:20 -0700

    Heavy changes to Warrior.cpp: Modified the attack class to better accomodate opponents of other types

--- Warrior.cpp
+++ Warrior.cpp
@@ -22,22 +22,31 @@ void Warrior::attack(Character &opponent) {
     // In order to access allegiance using the Character referenced passed in, we need to dynamic cast the Character reference to a Warrior reference
     // this dynamic casts a Character reference named opponent to Warrior reference named opp
     // without casting, we can't compare the allegiance's because Character class objects do not have a data member allegiance
-    Warrior &opp = dynamic_cast<Warrior &>(opponent);
 
-    // Warrior's do not attack other warriors of the same allegiance
-    // (if the strings are equal)
-    if (this->allegiance == opp.allegiance) {
-        cout << "Warrior " << this->name << " does not attack Warrior " << opp.name << "." << endl;
-        cout << "They share an allegiance with " << this->allegiance << endl;
+    // if the opponent's type is a warrior, then make a conversion so you can compare the allegiances
+    // REMEMBER, when we construct an object, we automatically pass in the corresponding hero type
+    // so the parameter opponent has an associated hero type. Let's compare that
+    // CANT CONVERT ANY OBJECT TYPE OTHER THAN WARRIOR TO A WARRIOR
+    // (if opponent is a WARRIOR then we can convert the character object, but if opponent is not a WARRIOR than we cannot convert)
+    if (opponent.getType() == WARRIOR) {
+        Warrior &opp = dynamic_cast<Warrior &>(opponent);
+
+        // compare the allegiance of the implicit parameter to the opp's (opponent) allegiance
+        // we have to do it within the if conditional because without Warrior object opp being declared we can't access another object's allegiance -- doesn't exist
+        // (i.e, no GetAllegiance() function)
+        if (this->allegiance == opp.allegiance) {
+            cout << "Warrior " << this->name << " does not attack Warrior " << opp.name << "." << endl;
+            cout << "They share an allegiance with " << this->allegiance << endl;
+        }
     } else {
         // damage done by Warrior is % of health remaining * warrior's attack strength
         double dmg = (this->health / MAX_HEALTH) * this->attackStrength;
 
         // attack dialogue
-        cout << "Warrior " << this->name << " attacks " << opp.name << " --- SLASH!!" << endl;
+        cout << "Warrior " << this->name << " attacks " << opponent.getName() << " --- SLASH!!" << endl;
 
         // attack the opponent
         // call damage() on opp object (the function will subtract that object's (the implicit param) health by variable dmg)
-        opp.damage(dmg);
+        opponent.damage(dmg);
     }
 }
